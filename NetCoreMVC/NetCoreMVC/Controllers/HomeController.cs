85d97250d32c104e1a0a832dda92a9360db97bc4
David Rodriguez <david_rodriguezsuarez@hotmail.com>
2018-09-13 12:43:09 -0600

    Commit Vistas Razor

---
+++ NetCoreMVC/NetCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreMVC.Models;
+using NetCoreMVC.ViewModels;
+using System.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NetCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {         
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Bethanya Pie Shop",
+                Pies = pies.ToList()
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ NetCoreMVC/NetCoreMVC/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NetCoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVC.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies{ get; set; }
+    }
+}
